512d09b077c6e4a41b7e64c6ae07fec0819fc094
Павел <pavelpolkovnikov334@gmail.com>
2021-03-30 20:45:15 +0700

    add printing message and button

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import telebot
-#from telebot import types
+from telebot import types
 
 #token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot('912680602:AAFuJ7VF3CuxO2_giada4gqGP_dwfLqkp5c')
@@ -11,17 +11,22 @@ def create_markup(keyboard, mod=0):
 
 @bot.message_handler(commands=['start', 'help'])
 def send_help(message):
-    bot.send_message(message.chat.id, 'тут типо помощь')
+    markup = types.ReplyKeyboardMarkup()
+    item_btn = types.KeyboardButton('Правила')
+    markup.row(item_btn)
+
+    bot.send_message(message.chat.id, 'тут типо помощь',
+                         reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
-    print(message.text)
+    print(f'Новое сообщение от {message.chat.first_name} {message.chat.last_name}: {message.text}')
     text = 'Сори я не понимаю тебя, пльзуйся кнопками'
     if message.text == 'Информация':
         text = 'Информация'
     elif message.text == 'Правила':
-        text = 'В отличие от других шопов, я НЕСУ ответственность за неудачный реф или вбив, будет перезаказ!!!'
+        text = 'В отличие от других шопов, я НЕСУ ответственность за неудачный реф или вбив, будет перезаказ!!! /help'
     elif message.text == 'Расценки':
         text = 'Цена не высока и варируется от кол-ва вашего заказа (об этом более подробно сказано в разделе "Информация") от 30 до 25%'
     bot.send_message(message.chat.id, text)
