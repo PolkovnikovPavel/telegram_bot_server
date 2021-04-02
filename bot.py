8430e9b51c2dcb7f8b0095af5116cab56197c741
Павел <pavelpolkovnikov334@gmail.com>
2021-04-02 23:44:14 +0700

    to server

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
 import os
-import telebot
+import telebot, pymysql
 from telebot import types
 
 #token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot('912680602:AAFuJ7VF3CuxO2_giada4gqGP_dwfLqkp5c')
 
+con = pymysql.connect(host="us-cdbr-east-03.cleardb.com", user="b40f50224688c7", passwd="d01ea3eb", db='heroku_80fffc474875cce')
+cur = con.cursor()
+
 
 def create_markup(keyboard, mod=0):
     pass
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_help(message):
     markup = types.ReplyKeyboardMarkup()
-    item_btn = types.KeyboardButton('Правила')
-    markup.row(item_btn)
+    item_btn1 = types.KeyboardButton('Правила')
+    item_btn2 = types.KeyboardButton('все данные из test1')
+    markup.row(item_btn1, item_btn2)
 
     bot.send_message(message.chat.id, 'тут типо помощь',
                          reply_markup=markup)
@@ -29,6 +34,8 @@ def echo_all(message):
         text = 'В отличие от других шопов, я НЕСУ ответственность за неудачный реф или вбив, будет перезаказ!!! /help'
     elif message.text == 'Расценки':
         text = 'Цена не высока и варируется от кол-ва вашего заказа (об этом более подробно сказано в разделе "Информация") от 30 до 25%'
+    elif message.text == 'все данные из test1':
+        text = 'все данные'
     bot.send_message(message.chat.id, text)
 
 
