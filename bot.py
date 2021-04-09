cb4c36dc719bb21e6edb2edfd8de97fa6ccb7be5
Павел <pavelpolkovnikov334@gmail.com>
2021-04-09 11:48:25 +0700

    Update db and create texts

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import telebot, pymysql, time
 from telebot import types
+from textes import *
 
 #token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot('912680602:AAFuJ7VF3CuxO2_giada4gqGP_dwfLqkp5c')
@@ -8,6 +9,27 @@ bot = telebot.TeleBot('912680602:AAFuJ7VF3CuxO2_giada4gqGP_dwfLqkp5c')
 con = pymysql.connect(host="us-cdbr-east-03.cleardb.com", user="b40f50224688c7", passwd="d01ea3eb", db='heroku_80fffc474875cce')
 cur = con.cursor()
 timer_con = time.time()
+
+
+def generate_payment_nonce(message):
+    num = str(time.time())[-5:]
+    return str(message.chat.id) + num
+
+
+def create_user(message):
+    cur.execute(f'''SELECT DISTINCT * FROM users
+    WHERE id = {message.chat.id}''')
+    result = cur.fetchall()
+
+    if not result:
+        payment_nonce = generate_payment_nonce(message)
+        inquiry = f"""INSERT INTO users
+VALUES ({message.chat.id}, '', '', '', '', '', '0', 0, 0, '80', '{payment_nonce}', 0)"""
+        cur.execute(inquiry)
+        con.commit()
+        print(f'добавлен новый пользователь, id = {message.chat.id}, зовут {message.chat.first_name} {message.chat.last_name}')
+        bot.send_message(message.chat.id, text_1,
+            reply_markup=create_markup([[button_1]]))
 
 
 def create_con():
@@ -29,28 +51,35 @@ def create_con():
 def check_timer_con(mod=0):
     if mod == 1:
         create_con()
-    if time.time() - timer_con > 600:
+    if time.time() - timer_con > 60:
         create_con()
 
 
 def create_markup(keyboard, mod=0):
-    pass
+    markup = types.ReplyKeyboardMarkup()
+    for line in keyboard:
+        items = []
+        for key in line:
+            items.append(types.KeyboardButton(key))
+        markup.row(*items)
+    return markup
 
 
-@bot.message_handler(commands=['start', 'help', 'reload', 'reset_con'])
+@bot.message_handler(commands=['start', 'help', 'reload_server_652431', 'reset_con'])
 def send_help(message):
-    if message.text == '/reload':
+    if message.text == '/reload_server_652431':
         bot.send_message(message.chat.id, 'Сейчас будет специально допущенна ошибка деления на 0, из за чего сервер падёт и после этого через 1-2 минуты перезагрузиться')
         print(1/0)
     elif message.text == '/reset_con':
         check_timer_con(1)
         bot.send_message(message.chat.id,
     'Переподключение к базе данных прошло успешно. И теперь в test1 есть об этом данные')
-    elif message.text == '/help' or message.text == 'start':
-        markup = types.ReplyKeyboardMarkup()
-        item_btn1 = types.KeyboardButton('Правила')
-        item_btn2 = types.KeyboardButton('все данные из test1')
-        markup.row(item_btn1, item_btn2)
+
+    elif message.text == '/start':
+        create_user(message)
+
+    elif message.text == '/help':
+        markup = create_markup([['Информация', 'Правила'], ['все данные из test1', 'все данные из users']])
 
         bot.send_message(message.chat.id, '''напиши "добавить : <int>;<int>;<str>", чтоб добавить новые данные в таблицу test1
     напиши "удалить : <int>", чтоб удалить выбранную строку из таблицы test1''',
@@ -75,6 +104,13 @@ def echo_all(message):
             results = cur.fetchall()
             for items in results:
                 text += str(items) + '\n'
+        elif message.text == 'все данные из users':
+            check_timer_con()
+            text = ''
+            cur.execute('''SELECT DISTINCT * FROM users''')
+            results = cur.fetchall()
+            for items in results:
+                text += str(items) + '\n'
         elif 'добавить :' in message.text:
             check_timer_con()
             num1, num2, str1 = message.text.split('добавить :')[-1].split(';')
@@ -87,7 +123,7 @@ def echo_all(message):
             check_timer_con()
             id = int(message.text.split('удалить :')[-1])
             cur.execute(f"""SELECT DISTINCT * FROM test1
-                            WHERE id = {id}""")
+                    WHERE id = {id}""")
             results = cur.fetchall()
             if results:
                 inquiry = f"""DELETE FROM test1
