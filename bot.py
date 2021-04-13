f33b1c5a2333e75a37ccea6c2e85584561c13473
Павел <pavelpolkovnikov334@gmail.com>
2021-04-13 10:10:10 +0700

    Update db and create texts

--- bot.py
+++ bot.py
@@ -11,25 +11,59 @@ cur = con.cursor()
 timer_con = time.time()
 
 
-def generate_payment_nonce(message):
+def generate_pay_code(message):
     num = str(time.time())[-5:]
     return str(message.chat.id) + num
 
 
-def create_user(message):
+def get_data_of_person(message):
+    check_timer_con()
     cur.execute(f'''SELECT DISTINCT * FROM users
     WHERE id = {message.chat.id}''')
     result = cur.fetchall()
+    if not result:
+        create_user(message, True)
+        cur.execute(f'''SELECT DISTINCT * FROM users
+    WHERE id = {message.chat.id}''')
+        result = cur.fetchall()
+    return result
+
+
+def create_user(message, mod=False):
+    if not mod:
+        cur.execute(f'''SELECT DISTINCT * FROM users
+        WHERE id = {message.chat.id}''')
+        result = cur.fetchall()
+    else:
+        result = False
 
     if not result:
-        payment_nonce = generate_payment_nonce(message)
+        pay_code = generate_pay_code(message)
         inquiry = f"""INSERT INTO users
-VALUES ({message.chat.id}, '', '', '', '', '', '0', 0, 0, '80', '{payment_nonce}', 0)"""
+VALUES ({message.chat.id}, '', '', '', '', '{pay_code}', '0', 0, 0, '80', '0', 0)"""
         cur.execute(inquiry)
         con.commit()
         print(f'добавлен новый пользователь, id = {message.chat.id}, зовут {message.chat.first_name} {message.chat.last_name}')
         bot.send_message(message.chat.id, text_1,
             reply_markup=create_markup([[button_1]]))
+
+
+def check_who_invited_me(who_invited_me):
+    check_timer_con()
+    cur.execute(f"""SELECT DISTINCT * FROM users
+        WHERE who_invited_me = '{who_invited_me}'""")
+    result = cur.fetchall()
+    if not result:
+        return False
+    return True
+
+
+def set_who_invited_me(message, who_invited_me):
+    inquiry = f"""UPDATE users
+            SET who_invited_me = '{who_invited_me}', type_menu = 1
+                WHERE id = '{message.chat.id}'"""
+    cur.execute(inquiry)
+    con.commit()
 
 
 def create_con():
@@ -89,14 +123,31 @@ def send_help(message):
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
     print(f'Новое сообщение от {message.chat.first_name} {message.chat.last_name}: {message.text}')
+    data_of_person = get_data_of_person(message)[0]
     text = 'Сори я не понимаю тебя, пльзуйся кнопками'
+    markup = None
     try:
-        if message.text == 'Информация':
-            text = 'Информация'
-        elif message.text == 'Правила':
-            text = 'В отличие от других шопов, я НЕСУ ответственность за неудачный реф или вбив, будет перезаказ!!! /help'
-        elif message.text == 'Расценки':
-            text = 'Цена не высока и варируется от кол-ва вашего заказа (об этом более подробно сказано в разделе "Информация") от 30 до 25%'
+        if data_of_person[11] == 0:
+            if message.text != button_1:
+                if check_who_invited_me(message.text):
+                    set_who_invited_me(message, message.text)
+                    text = text_3
+                    markup = create_markup([[button_2]])
+                else:
+                    text = text_2
+                    markup = create_markup([[button_1]])
+            else:
+                set_who_invited_me(message, '0')
+                text = text_3
+                markup = create_markup([[button_2]])
+
+        if data_of_person[11] == 1:
+            if message.text == button_2:
+                pass   # перейти в штатный режим
+            else:
+                text = text_4
+                markup = create_markup([[button_2]])
+
         elif message.text == 'все данные из test1':
             check_timer_con()
             text = ''
@@ -137,7 +188,10 @@ def echo_all(message):
     except Exception:
         text = 'что-то пошло не так. Скорее всего потеряно соединение с сервером MySQL во время запроса. Попробуйте ещё раз, а мы пока наладим соединение'
         check_timer_con(1)
-    bot.send_message(message.chat.id, text)
+    if markup:
+        bot.send_message(message.chat.id, text, reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
